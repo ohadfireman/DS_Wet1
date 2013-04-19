26b12a31f659bd5b1e3f42b623419ff3835f9a65
Aviad Rom <b2faviad@Aviads-MacBook-Pro.local>
2013-04-19 08:36:54 +0300

    Initial Commit

---
+++ Mivne1/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Mivne1
+//
+//  Created by Aviad Rom on 4/19/13.
+//  Copyright (c) 2013 Aviad Rom. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
